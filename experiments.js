32b18420b4abed5118d3705298ee923cfcd4d9a0
Luiz Henrique <luizhjramos@outlook.com>
2022-01-05 21:58:11 -0300

    feat: Simulating the javascript execution stack with the Loop event

--- experiments.js
+++ experiments.js
@@ -1,1 +1,17 @@
-console.log("projectInitialize")
+const fn3 = () => {
+    console.log("fn3")
+}
+
+const fn2 = () => {
+    fn3()
+}
+
+const fn1 = () => {
+    fn2()
+}
+
+const main = () => {
+    fn1()
+}
+
+main()
